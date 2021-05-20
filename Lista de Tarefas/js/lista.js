74359d6435bc61060f9ee8abab81e7410eb9c5fb
Lyllys <lyllysgalhardo@gmail.com>
2021-05-20 20:51:40 -0300

    Aterações finais ( de verdade!)

--- Lista de Tarefas/js/lista.js
+++ Lista de Tarefas/js/lista.js
@@ -1,31 +1,31 @@
 const form = document.querySelector('form');
-        const input = document.querySelector('#adicionar-tarefas');
-        const ul = document.querySelector('ul');
+const input = document.querySelector('#adicionar-tarefas');
+const ul = document.querySelector('ul');
 
 
-        form.addEventListener('submit', function (evento) {
+form.addEventListener('submit', function (evento) {
 
-            evento.preventDefault();
+    evento.preventDefault();
 
-            const li = document.createElement('li');
-            const botaoExcluir = document.createElement('button')
+    const li = document.createElement('li');
+    const botaoExcluir = document.createElement('button')
 
-            li.classList.add('list-group-item');
-            botaoExcluir.classList.add('btn', 'btn-outline-danger');
+    li.classList.add('list-group-item');
+    botaoExcluir.classList.add('btn', 'btn-outline-danger');
 
-            li.textContent = input.value;
-            input.value = '';
+    li.textContent = input.value;
+    input.value = '';
 
-            botaoExcluir.textContent = 'x';
+    botaoExcluir.textContent = 'x';
 
-            ul.append(li);
-            li.append(botaoExcluir);
+    ul.append(li);
+    li.append(botaoExcluir);
 
-            botaoExcluir.addEventListener('click', function () {
-                const excluir = document.querySelector('li');
+    botaoExcluir.addEventListener('click', function () {
+        const excluir = document.querySelector('li');
 
-                li.remove();
+        li.remove();
 
-            })
+    })
 
-        })
+})
